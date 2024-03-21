3058e7684e10c14de7b271b997b6d14c10189750
Timazay <oxxxymofey@gmail.com>
2024-03-21 19:45:00 +0300

    final

--- src/by/itstep/javalessons/lesson34/task01/controller/Main.java
+++ src/by/itstep/javalessons/lesson34/task01/controller/Main.java
@@ -1,7 +1,13 @@
 package by.itstep.javalessons.lesson34.task01.controller;
 
+import by.itstep.javalessons.lesson34.task01.model.entity.WeekDay;
+
 public class Main {
     public static void main(String[] args) {
-        
+        WeekDay monday = new WeekDay();
+        WeekDay friday = new WeekDay("Friday", 5);
+
+        System.out.println(monday);
+        System.out.println(friday);
     }
 }
